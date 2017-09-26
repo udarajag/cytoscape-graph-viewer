b53a652c516c0ca0ea70506b9bf5c7cf834de0cd
Udara <udarajag@gmail.com>
2017-09-26 22:16:52 +0800

    initial commit on graph

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/* AJAX send function */
+function sendAjax(method, url, data, success, error) {
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: method,
+        url: url,
+        data: JSON.stringify(data),
+        dataType: 'json',
+        success: success,
+        error: error
+    });
+}
+
+function prettyJson(json) {
+    if (typeof json !== 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
---
+++ js/ontologyView.js
@@ -0,0 +1,3 @@
+function initialize() {
+    searchByQuery("match (n1:OntConcept)-[r1]->(n2) return r1,n1,n2 limit 25");
+}
