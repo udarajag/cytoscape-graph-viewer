132158dd6e079b2f381fcddd36f90cb5066d881b
Ureka <Ureka@192.168.0.102>
2017-10-11 22:41:06 +0800

    Ontology visualization

--- js/ontologyView.js
+++ js/ontologyView.js
@@ -1,3 +1,3 @@
 function initialize() {
-    searchByQuery("match (n1:OntConcept)-[r1]->(n2) return r1,n1,n2 limit 25");
+    searchByQuery("match (n1:Class)-[r1]->(n2) return r1,n1,n2 limit 25");
 }
